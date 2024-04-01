369b3fd32854ca405da2ba3298e0e5d0d45ee535
Andy Wang <andy_wange347@hotmail.com>
2024-03-31 19:38:42 -0700

    Added a python backend api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
 import ChatGPTComponent from "./components/chatGPT";
+import { React, useState, useEffect } from "react";
 
 function App() {
+  const [test, setTest] = useState("test");
+
+  useEffect(() => {
+    fetch("/api/test")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setTest(data.message);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <ChatGPTComponent />
+        <p>{test}</p>
       </header>
     </div>
   );
